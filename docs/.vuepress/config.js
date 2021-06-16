48e8fe4e15ca9717d4360046f7142d9325daa0b3
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2021-06-16 23:04:29 +0200

    Potential fix for android/firefox problem

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
