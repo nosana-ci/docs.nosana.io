ef04b917dfddbe19a27ef9d48fc55bff8b12a66f
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2021-12-15 15:30:15 +0100

    Update token description

--- docs/.vuepress/components/TokenDescription.vue
+++ docs/.vuepress/components/TokenDescription.vue
@@ -21,22 +21,22 @@ export default {
 
       const pool = this.getPool()
 
-      let description = `${pool.name} tokens take up ${pool.total * 100}% of the total amount of ${ticker} tokens.`
+      let description = `${pool.name} tokens account for ${pool.total * 100}% of the total number of ${ticker} tokens.`
 
       if (pool.vesting === 0 && pool.lock === 0) {
         description += ` These tokens are available at token distribution.`
         return description
       }
       if (pool.vesting !== 0) {
-        description += ` The tokens are linearly released over ${pool.vesting} months (${pool.vesting / 12} years), `
+        description += ` The tokens are released in a linear fashion over a period of ${pool.vesting} months (${pool.vesting / 12} years), `
         if (pool.lock !== 0) {
           description += ` after they have been locked for ${pool.lock} months.`
         } else {
-          description += ` starting at token distribution.`
+          description += ` beginning with at token distribution.`
         }
       }
       if (pool.upfront !== 0) {
-        description += ` ${pool.upfront * 100}% of this pool is directly released at token distribution.`
+        description += ` At token distribution, ${pool.upfront * 100}% of this pool is directly released.`
       }
 
       return description
