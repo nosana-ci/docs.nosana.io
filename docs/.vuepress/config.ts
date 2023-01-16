aafb6d2b8d61237d58412ac0e79fdeb4f805d521
Sjoerd Dijkstra <sjoerd@yogy.io>
2023-01-16 17:22:35 +0100

    Update hope theme

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,15 +1,11 @@
 import { defineUserConfig } from 'vuepress';
-import { addViteSsrNoExternal } from 'vuepress-shared';
 import theme from './theme.js';
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Nosana Documentation',
-  description: 'Next Gen CI/CD',
+  description: 'The Web3 trusted DevOps platform',
   base: '/',
   theme,
   shouldPrefetch: false,
-  extendsBundlerOptions: (config, app) => {
-    addViteSsrNoExternal({ app, config }, 'vuepress-shared');
-  },
 });
