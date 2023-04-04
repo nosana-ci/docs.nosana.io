ec32b547f6ed5421a754b416780ac5844afdbc3b
Sjoerd Dijkstra <sjoerd@yogy.io>
2023-04-04 16:54:39 +0200

    Enable docsearch

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from 'vuepress';
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import theme from './theme.js';
 
 export default defineUserConfig({
@@ -8,4 +9,11 @@ export default defineUserConfig({
   base: '/',
   theme,
   shouldPrefetch: false,
+  plugins: [
+    docsearchPlugin({
+      appId: 'O80DOX8SB5',
+      apiKey: '4cda0685d3eac8d69041dd61f5d5d68e',
+      indexName: 'nosana',
+    })
+  ]
 });
